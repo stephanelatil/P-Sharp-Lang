c968d2db13bac2b4f6de6574a3ae0759416bf9e0
Stephane Latil <stephanelatil@gmail.com>
2025-01-02 09:06:38 +0100

    Updates operations

--- compiler2/operations.py
+++ compiler2/operations.py
@@ -1,36 +1,38 @@
-from enum import Enum
+from enum import Enum, auto
 
 class BinaryOperation(Enum):
-    PLUS = '+'
-    MINUS = '-'
-    TIMES = '*'
-    DIVIDE = '/'
-    MOD = '%'
-    BOOL_EQ = "=="
-    BOOL_NEQ = "!="
-    BOOL_GEQ = ">="
-    BOOL_LEQ = "<="
-    BOOL_GT = ">"
-    BOOL_LT = "<"
-    BOOL_AND = "and"
-    BOOL_OR = "or"
-    LOGIC_AND = "&"
-    LOGIC_OR = "|"
-    XOR = "^"
-    SHIFT_LEFT = "<<"
-    SHIFT_RIGHT = ">>"
-    ASSIGN="assign"
-    COPY="copy"
+    PLUS = auto()
+    MINUS = auto()
+    TIMES = auto()
+    DIVIDE = auto()
+    MOD = auto()
+    BOOL_EQ = auto()
+    BOOL_NEQ = auto()
+    BOOL_GEQ = auto()
+    BOOL_LEQ = auto()
+    BOOL_GT = auto()
+    BOOL_LT = auto()
+    BOOL_AND = auto()
+    BOOL_OR = auto()
+    LOGIC_AND = auto()
+    LOGIC_OR = auto()
+    XOR = auto()
+    SHIFT_LEFT = auto()
+    SHIFT_RIGHT = auto()
+    ASSIGN = auto()
+    COPY = auto()
 
     def __repr__(self):
         return f"\"BinaryOperation.{self.name}\""
 
 class UnaryOperation(Enum):
-    MINUS = '-'
-    BOOL_NOT = "not"
-    LOGIC_NOT = "!"
-    INCREMENT = "++"
-    DECREMENT = "--"
+    MINUS = auto()
+    BOOL_NOT = auto()
+    LOGIC_NOT = auto()
+    POST_INCREMENT = auto()
+    POST_DECREMENT = auto()
+    PRE_INCREMENT = auto()
+    PRE_DECREMENT = auto()
 
     def __repr__(self):
         return f"\"UnaryOperation.{self.name}\""
