1bbe8faf7bb165ba033118e10b7f59546997f428
Stephane Latil <stephanelatil@gmail.com>
2025-02-04 10:13:53 +0100

    Changes binary ops values

--- compiler2/operations.py
+++ compiler2/operations.py
@@ -1,26 +1,26 @@
 from enum import Enum, auto
 
 class BinaryOperation(Enum):
-    PLUS = auto()
-    MINUS = auto()
-    TIMES = auto()
-    DIVIDE = auto()
-    MOD = auto()
-    BOOL_EQ = auto()
-    BOOL_NEQ = auto()
-    BOOL_GEQ = auto()
-    BOOL_LEQ = auto()
-    BOOL_GT = auto()
-    BOOL_LT = auto()
-    BOOL_AND = auto()
-    BOOL_OR = auto()
-    LOGIC_AND = auto()
-    LOGIC_OR = auto()
-    XOR = auto()
-    SHIFT_LEFT = auto()
-    SHIFT_RIGHT = auto()
-    ASSIGN = auto()
-    COPY = auto()
+    PLUS = '+'
+    MINUS = '-'
+    TIMES = '*'
+    DIVIDE = '/'
+    MOD = '%'
+    BOOL_EQ = '=='
+    BOOL_NEQ = '!='
+    BOOL_GEQ = '>='
+    BOOL_LEQ = '<='
+    BOOL_GT = '>'
+    BOOL_LT = '<'
+    BOOL_AND = 'and'
+    BOOL_OR = 'or'
+    LOGIC_AND = '&'
+    LOGIC_OR = '|'
+    XOR = '^'
+    SHIFT_LEFT = "<<"
+    SHIFT_RIGHT = ">>"
+    ASSIGN = "="
+    COPY = ':='
 
     def __repr__(self):
         return f"\"BinaryOperation.{self.name}\""
