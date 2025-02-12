3292e58fd4c18a8788f09605896c042452f31e2f
Stephane Latil <stephanelatil@gmail.com>
2025-02-12 10:22:24 +0100

    Adds test builder

---
+++ garbage_collector/build_tests.sh
@@ -0,0 +1,10 @@
+COMPILER=clang
+
+GC_OBJ=gc.o
+TESTS_OBJ=tests.o
+
+TESTS_EXEC=tests.out
+
+${COMPILER} -Wall -g -O0 -c -o ${GC_OBJ} gc.c
+${COMPILER} -Wall -g -O0 -c -o ${TESTS_OBJ} tests/tests.c
+${COMPILER} -g -O0 ${GC_OBJ} ${TESTS_OBJ} -o ${TESTS_EXEC}
