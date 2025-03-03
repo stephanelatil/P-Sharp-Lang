22805cd53b38268fe38412e9674d41fa3f1558ab
Stephane Latil <stephanelatil@gmail.com>
2025-03-03 14:21:21 +0100

    script edits

--- garbage_collector/compile_GC_to_library.sh
+++ garbage_collector/compile_GC_to_library.sh
@@ -1,10 +1,10 @@
 SOURCE_FILE=gc.c
 LIB_NAME=libps_gc.so
-LIB_NAME_ll=libps_gc.ll
+LIB_NAME_LL=libps_gc.ll
 LIB_NAME_BC=libps_gc.bc
 FLAGS=-Wall
 COMPILER=clang-15
 
 ${COMPILER} -shared ${FLAGS} -o ${LIB_NAME} -fPIC ${SOURCE_FILE}
-${COMPILER} -O3 -emit-llvm ${SOURCE_FILE} -S -o ${LIB_NAME_BC}
+${COMPILER} -O3 -emit-llvm ${SOURCE_FILE} -S -o ${LIB_NAME_LL}
 ${COMPILER} -O3 -emit-llvm ${SOURCE_FILE} -c -o ${LIB_NAME_BC}
