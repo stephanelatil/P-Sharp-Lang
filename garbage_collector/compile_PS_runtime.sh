9126997f8d5865f766d3b35f114e26a980d4c7f9
Stephane Latil <stephanelatil@gmail.com>
2025-04-05 18:39:27 +1300

    Only compiled runtime to BC

--- garbage_collector/compile_PS_runtime.sh
+++ garbage_collector/compile_PS_runtime.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-set -e
+set -v -e
 
 GC_FILENAME=gc
 UTILS_FILENAME=ps_utils
@@ -8,7 +8,7 @@ RENAME_PASS_PY=rename_pass.py
 RENAME_MAPPING_CSV=rename_mapping.csv
 FLAGS=-Wall
 
-OPT=opt-15
+PY="../.venv/bin/python3"
 COMPILER=clang-15
 LLVM_LIB_INCLUDES="-I /usr/include/llvm-15 -I /usr/include/llvm-c-15"
 LLVM_LINKER=llvm-link-15
@@ -20,8 +20,6 @@ ${COMPILER} -O3 -emit-llvm -c ${FLAGS} -o "${UTILS_FILENAME}.bc" "${UTILS_FILENA
 ${LLVM_LINKER} "${GC_FILENAME}.bc" "${UTILS_FILENAME}.bc" -o "${COMBINED_FILENAME}.bc"
 
 # rename symbols to adhere to standard naming
-python3 "${RENAME_PASS_PY}" "${COMBINED_FILENAME}.bc" "${RENAME_MAPPING_CSV}"
+${PY} "${RENAME_PASS_PY}" "${COMBINED_FILENAME}.bc" "${RENAME_MAPPING_CSV}"
 
-${COMPILER} -I"${LLVM_LIBS}" -c  -o "${COMBINED_FILENAME}.o" "${COMBINED_FILENAME}.bc"
-
-rm "${GC_FILENAME}.bc" "${UTILS_FILENAME}.bc" "${COMBINED_FILENAME}.bc"
+rm "${GC_FILENAME}.bc" "${UTILS_FILENAME}.bc"
